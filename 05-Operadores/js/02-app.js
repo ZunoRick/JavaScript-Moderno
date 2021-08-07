7fd93cc2ac2d6880563165eac2c2f020d8dba978
ZunoRick <zunosan.ricardo506@gmail.com>
2021-08-07 16:18:41 -0500

    Agregando archivos de Operadores

---
+++ 05-Operadores/js/02-app.js
@@ -0,0 +1,13 @@
+const numero1 = 20;
+const numero2 = "20";
+const numero3 = 30;
+
+console.log(numero1 == numero2);
+console.log(numero1 === numero2);
+
+const password1 = "admin";
+const password2 = "Admin";
+console.log(password1 != password2);
+
+console.log(numero1 != numero2);
+console.log(numero1 !== numero2);
