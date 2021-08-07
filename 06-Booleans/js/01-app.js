3e8cba1a1782bd14caa6ed7f5943ab07bbe8c011
ZunoRick <zunosan.ricardo506@gmail.com>
2021-08-07 16:32:50 -0500

    Agregando archivos de booleans

---
+++ 06-Booleans/js/01-app.js
@@ -0,0 +1,8 @@
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "true";
+
+console.log(boolean1 == boolean3);
+
+const boolean4 = new Boolean(true);
+console.log(boolean4);
---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,9 @@
+const autenticado = true;
+
+if (autenticado) {
+    console.log('Si puedes ver Netflix');
+} else{
+    console.log('No, no puedes ver Netflix');
+}
+
+console.log( autenticado ? 'Usuario autenticado' : 'No autenticado');
