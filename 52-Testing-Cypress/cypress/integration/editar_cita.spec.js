49c3eec70c612a37f1174f53ad2799b55f8e2e5b
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2021-10-11 19:11:10 -0500

    Agregando archivos de Testing con Cypress

---
+++ 52-Testing-Cypress/cypress/integration/editar_cita.spec.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress"/>
+
+describe('Llena los campos para una nueva cita y la edita', () => {
+	it('Campos nueva cita', () => {
+		cy.visit('/index.html');
+		cy.get('[data-cy="mascota-input"]').type('Hook');
+		cy.get('[data-cy="propietario-input"]').type('Ricardo');
+		cy.get('[data-cy="telefono-input"]').type('55884466');
+		cy.get('[data-cy="fecha-input"]').type('2021-10-11');
+		cy.get('[data-cy="hora-input"]').type('15:00');
+		cy.get('[data-cy="sintomas-textarea"]').type('El gato solo come y duerme');
+		cy.get('[data-cy="submit-cita"]').click();
+
+		cy.get('[data-cy="citas-heading"]')
+			.invoke('text')
+			.should('equal', 'Administra tus Citas');
+
+		//Seleccionar la alerta
+		cy.get('[data-cy="alerta"]')
+			.invoke('text')
+			.should('equal', 'Se agregó correctamente');
+
+		cy.get('[data-cy="alerta"]').should('have.class', 'alert-success');
+	});
+
+	it('Edita la cita', () => {
+		cy.get('[data-cy="btn-editar"]').click();
+		cy.get('[data-cy="mascota-input"]').clear().type('Nuevo Hook');
+		cy.get('[data-cy="submit-cita"]').click();
+
+		//Seleccionar la alerta
+		cy.get('[data-cy="alerta"]')
+			.invoke('text')
+			.should('equal', 'Guardado Correctamente');
+		cy.get('[data-cy="alerta"]').should('have.class', 'alert-success');
+        
+        cy.screenshot();
+	});
+});
